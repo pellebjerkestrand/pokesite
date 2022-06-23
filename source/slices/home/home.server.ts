645f1e686fd7413f48c9910bcbc2e30c7ddcca87
Pelle Bjerkestrand <pellebjerkestrand@users.noreply.github.com>
2022-06-23 13:53:01 +0200

    only caches when necessary.

--- source/slices/home/home.server.ts
+++ source/slices/home/home.server.ts
@@ -4,24 +4,36 @@ import { queueName } from "./response-cache-queue-helpers";
 import { renderHtml, RenderHtmlOutcome } from "../../server/render-html";
 
 import { Home } from "./home.page";
-import { getAllPokemon } from "./pokemon";
+import { getAllPokemon, GetAllPokemonOutcome, PokemonSource } from "./pokemon";
 
 export const run: HtmlFunction = async (context) => {
   const pokemon = await getAllPokemon();
 
+  if (pokemon.outcome === GetAllPokemonOutcome.failure) {
+    return {
+      body: "<p>Oh, dear. Something went wrong when getting Pokémon.</p>",
+      headers: {
+        "content-type": "text/html",
+      },
+      status: 500,
+    };
+  }
+
+  if (pokemon.source === PokemonSource.pokeApi) {
+    context.bindings[queueName] = Object.keys(pokemon.value);
+  }
+
   const result = renderHtml(Home, "Pokémon", {
-    list: Object.values(pokemon).flatMap((list) =>
+    list: Object.values(pokemon.value).flatMap((list) =>
       list.map(({ name }) => name)
     ),
     title: "Pokémon",
   });
 
-  context.bindings[queueName] = Object.keys(pokemon);
-
   switch (result.outcome) {
     case RenderHtmlOutcome.Failure: {
       return {
-        body: "<p>Oh, dear.</p>",
+        body: "<p>Oh, dear. Something went wrong when rendering HTML.</p>",
         headers: {
           "content-type": "text/html",
         },
--- source/slices/home/pokemon.ts
+++ source/slices/home/pokemon.ts
@@ -1,6 +1,6 @@
-import { type FromDecoder } from "ts.data.json";
+import { JsonDecoder, type FromDecoder } from "ts.data.json";
 
-import { get, GetOutcome } from "../../server/data";
+import { get, getFromCache, GetOutcome, set } from "../../server/data";
 import { resultsDecoder, resourceListDecoder } from "./resource-list";
 
 export enum GetAllPokemonOutcome {
@@ -8,29 +8,82 @@ export enum GetAllPokemonOutcome {
   success = "success",
 }
 
-type Collection = Record<string, FromDecoder<typeof resultsDecoder>>;
+export enum PokemonSource {
+  pageCache = "pageCache",
+  pokeApi = "pokeApi",
+}
 
-export const getAllPokemon = async (
+const collectionDecoder = JsonDecoder.dictionary(resultsDecoder, "Collection");
+
+type Collection = FromDecoder<typeof collectionDecoder>;
+
+const cacheKey = "all-pokemon";
+
+const getFreshPokemon = async (
   url: string = "https://pokeapi.co/api/v2/pokemon",
   collection: Collection = {}
-): Promise<Collection> => {
+): Promise<Collection | undefined> => {
   const result = await get(url, resourceListDecoder);
 
   switch (result.outcome) {
-    case GetOutcome.failure: {
-      return {};
-    }
+    case GetOutcome.failure:
     case GetOutcome.invalidKey: {
-      return {};
+      return;
     }
     case GetOutcome.success: {
       collection[url] = result.value.results;
 
       if (result.value.next) {
-        return getAllPokemon(result.value.next, collection);
+        return getFreshPokemon(result.value.next, collection);
       }
 
+      await set(cacheKey, collection);
       return collection;
     }
   }
+};
+
+const getCachedPokemon = async (): Promise<Collection | undefined> => {
+  const result = await getFromCache(cacheKey, collectionDecoder);
+
+  switch (result.outcome) {
+    case GetOutcome.failure:
+    case GetOutcome.invalidKey: {
+      return;
+    }
+    case GetOutcome.success: {
+      return result.value;
+    }
+  }
+};
+
+export const getAllPokemon = async (): Promise<
+  | {
+      outcome: GetAllPokemonOutcome.success;
+      source: PokemonSource;
+      value: Collection;
+    }
+  | {
+      outcome: GetAllPokemonOutcome.failure;
+    }
+> => {
+  const cachedPokemon = await getCachedPokemon();
+  if (cachedPokemon) {
+    return {
+      outcome: GetAllPokemonOutcome.success,
+      source: PokemonSource.pageCache,
+      value: cachedPokemon,
+    };
+  }
+
+  const freshPokemon = await getFreshPokemon();
+  if (freshPokemon) {
+    return {
+      outcome: GetAllPokemonOutcome.success,
+      source: PokemonSource.pageCache,
+      value: freshPokemon,
+    };
+  }
+
+  return { outcome: GetAllPokemonOutcome.failure };
 };
